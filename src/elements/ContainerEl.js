face06a01b644c1b3d3c5813575c5377504993b4
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-05-26 21:31:32 +0300

    feat: created Container and ContainerEl

---
+++ src/elements/ContainerEl.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const ContainerWrap = styled.div`
+  height: 100%;
+  max-width: 1200px;
+  padding: 0 40px;
+  margin: 0 auto;
+`
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Container } from '../components/Container'
+
+export default () => (
+  <Container>
+    <h1>Hello Gatsby!</h1>
+  </Container>
+)
