389da3bd28f054bde3e57a51a55706f590dc1726
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-05-29 20:57:10 +0300

    fix: fix tetlify problem

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,47 +1,47 @@
-exports.createPages = async ({ actions, graphql }) => {
-  const { data } = await graphql`
-    query {
-      allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-        edges {
-          node {
-            frontmatter {
-              slug
-            }
-            id
-          }
-        }
-      }
-    }
-  `
+// exports.createPages = async ({ actions, graphql }) => {
+//   const { data } = await graphql`
+//     query {
+//       allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+//         edges {
+//           node {
+//             frontmatter {
+//               slug
+//             }
+//             id
+//           }
+//         }
+//       }
+//     }
+//   `
 
-  // Create paginated
+//   // Create paginated
 
-  const postPerPage = 3
-  const numPages = Math.ceil(data.allMdx.edges.length / postPerPage)
+//   const postPerPage = 3
+//   const numPages = Math.ceil(data.allMdx.edges.length / postPerPage)
 
-  Array.from({ length: numPages }).forEach((item, i) => {
-    actions.createPages({
-      path: i === 0 ? '/' : `/${i + 1}`,
-      components: require.resolve('./src/templates/AllPosts.jsx'),
-      constext: {
-        limit: postPerPage,
-        skip: i * postPerPage,
-        numPages,
-        currentPage: i + 1
-      }
-    })
-  })
+//   Array.from({ length: numPages }).forEach((item, i) => {
+//     actions.createPages({
+//       path: i === 0 ? '/' : `/${i + 1}`,
+//       components: require.resolve('./src/templates/AllPosts.jsx'),
+//       constext: {
+//         limit: postPerPage,
+//         skip: i * postPerPage,
+//         numPages,
+//         currentPage: i + 1
+//       }
+//     })
+//   })
 
-  // create single blog posts
+//   // create single blog posts
 
-  data.allMdx.edges.forEach(edge => {
-    const slug = edge.node.frontmatter.slug
-    const id = edge.node.id
-    actions.createPages({
-      path: slug,
-      omponents: require.resolve('./src/templates/SinglePost.jsx'),
-      context: { id }
-    })
-  })
+//   data.allMdx.edges.forEach(edge => {
+//     const slug = edge.node.frontmatter.slug
+//     const id = edge.node.id
+//     actions.createPages({
+//       path: slug,
+//       omponents: require.resolve('./src/templates/SinglePost.jsx'),
+//       context: { id }
+//     })
+//   })
 
-}
+// }
